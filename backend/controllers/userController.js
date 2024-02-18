2076b19b177851377170f210503f7745aba64ec1
dhavalmak77 <dhavalmak77@gmail.com>
2024-02-18 17:13:11 +0530

    complete app

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -14,9 +14,6 @@ const getUser = async (req, res) => {
 		} else {
 			res.status(404).json({ message: 'Invalid user session!' });
 		}
-		// const user = await User.findOne({}).populate('orders');
-
-		// return res.status(200).json({ message: 'User created successfully..', data: user });
 	} catch (error) {
 		console.error('Error occured while fetching user: ', error);
 		return res.status(500).json({ message: error.message });
